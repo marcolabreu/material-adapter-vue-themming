484668b6f7daf6e4c4a79712b88804b25092fe92
Marco L. Abreu <marcolabreu@royalmail.top>
2020-08-19 14:54:00 +0200

    Upgrade vue-material-adapter to now working 0.17.0-beta.10

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import "./styles";
 import Vue from "vue";
 import App from "./App.vue";
+import VueCompositionAPI from "@vue/composition-api";
 import VueMaterialAdapter from "vue-material-adapter";
 
+Vue.use(VueCompositionAPI);
 Vue.config.productionTip = false;
+
 Vue.use(VueMaterialAdapter);
 
 new Vue({
